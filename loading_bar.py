b9114dc99a7caeb7cf61aaf49ee16000b3d44e07
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2024-07-12 15:29:11 -0500

    Update loading_bar.py

--- loading_bar.py
+++ loading_bar.py
@@ -1,7 +1,5 @@
-import sys
-
 class LoadingBar:
-    def __init__(self, max_value, length=50, show_percentage=True, show_progress=True, filled='#', empty='-'):
+    def __init__(self, max_value, length=50, show_percentage=True, show_progress=True, filled='#', empty='-', starting_operation = ''):
         self.value = 0
         self.max_value = max_value
         self.length = length
@@ -9,8 +7,9 @@ class LoadingBar:
         self.show_progress = show_progress
         self.filled = filled
         self.empty = empty
-
-    def display(self):
+        self.current_operation = starting_operation
+        self.previous_length = 0
+    def _display(self):
         percent_filled = round(self.value / self.max_value, 2)
         filled_cells = round(self.length * percent_filled)
         bar_string = '\r'
@@ -18,9 +17,13 @@ class LoadingBar:
             bar_string += f'[{self.value}{" " * (len(str(self.max_value))-len(str(self.value)))}/{self.max_value}]  '
         if self.show_percentage:
             bar_string += f'[{round(percent_filled * 100)}{"  " if percent_filled<0.1 else " " if percent_filled<1 else ""}%]  '
-        bar_string += f'[{self.filled * filled_cells}{self.empty * (self.length-filled_cells)}]'
+        bar_string += f'[{self.filled * filled_cells}{self.empty * (self.length-filled_cells)}]  '
+        bar_string += self.current_operation
+        if self.previous_length > len(bar_string):
+            print(f'\r{" "*self.previous_length}', end='')
         print(bar_string, end='')
-    def end(self):
+        self.previous_length = len(bar_string)
+    def _end(self):
         percent_filled = self.value / self.max_value
         filled_cells = round(self.length * percent_filled)
         bar_string = '\r'
@@ -29,10 +32,12 @@ class LoadingBar:
         if self.show_percentage:
             bar_string += f'[{round(percent_filled * 100)}{"  " if percent_filled<0.1 else " " if percent_filled<1 else ""}%]  '
         bar_string += f'[{self.filled * filled_cells}{self.empty * (self.length-filled_cells)}]'
+        print(f'\r{" "*self.previous_length}', end='')
         print(bar_string)
-    def update(self, new_value=0, increment=False):
+    def update(self, current_operation='', new_value=0, increment=False):
         self.value = new_value if not increment else self.value + 1
+        self.current_operation = current_operation if current_operation != '' else self.current_operation
         if self.value < self.max_value:
-            self.display()
+            self._display()
         else:
-            self.end()
+            self._end()
