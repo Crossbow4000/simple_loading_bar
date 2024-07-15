f55d7d2675b98830f183497908179d0355cadfe9
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2024-07-15 11:35:00 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='loading_bar',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A simple loading bar for console applications',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
